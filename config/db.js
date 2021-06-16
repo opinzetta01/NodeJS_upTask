fe414c4c1ada550b7e05f273b7b9f532c4a02197
opinzetta01 <opinzetta@gmail.com>
2021-06-16 00:35:01 -0300

    copia

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require ('sequelize');
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({path: 'variables.env'});
 
 const db = new Sequelize(
   process.env.DB_NAME,
